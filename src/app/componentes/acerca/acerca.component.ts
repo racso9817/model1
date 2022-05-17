6581aac2a2a96b2e1d45f974f83fc1a988ce37ed
Oscar Avila <oscaravilaa9817@gmail.com>
2022-05-16 23:22:26 -0500

    siuuuuu

--- src/app/componentes/acerca/acerca.component.ts
+++ src/app/componentes/acerca/acerca.component.ts
@@ -5,14 +5,13 @@ import {NgbActiveModal, NgbModal} from '@ng-bootstrap/ng-bootstrap';
   selector: 'ngbd-modal-content',
   template: `
     <div class="modal-header">
-      <h4 class="modal-title">{{name}}</h4>
-      <button type="button" class="btn-close" aria-label="Close" (click)="activeModal.dismiss('Cross click')"></button>
+      <h1 class="modal-title">{{name}}</h1>      
     </div>
     <div class="modal-image text-center mt-3">
       <img src="{{image}}" alt="">
     </div>
     <div class="modal-body text-center">
-      <p>{{description}}</p>
+      <h3>{{description}}</h3>
     </div>
     <div class="modal-footer">
       <button type="button" class="btn btn-outline-dark" (click)="activeModal.close('Close click')">Cerrar</button>
@@ -34,7 +33,7 @@ export class NgbdModalContent {
   templateUrl: './acerca.component.html',
   styleUrls: ['./acerca.component.sass']
 })
-export class AcercaComponent {
+export class AcercaComponent implements OnInit {
 
   cardsList = [
     {
@@ -66,6 +65,39 @@ export class AcercaComponent {
     modalRef.componentInstance.name = this.cardsList[num].name;
     modalRef.componentInstance.image = this.cardsList[num].img;
     modalRef.componentInstance.description = this.cardsList[num].desc; 
+  }
+
+  //animation for title
+  // observer = new IntersectionObserver(entries => {
+  //   // Loop over the entries
+  //   entries.forEach(entry => {
+  //     // If the element is visible
+  //     if (entry.isIntersecting) {
+  //       // Add the animation class
+  //       entry.target.classList.add('animated');
+  //     }
+  //   });
+  // });
+
+  reveal(){
+    var reveals = document.querySelectorAll('#title');
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+
+  ngOnInit() {
+    //this.observer.observe(document.querySelectorAll('#title')[0]);
+    window.addEventListener("scroll", this.reveal);
+    // To check the scroll position on page load
+    this.reveal();
   }
 
 }
